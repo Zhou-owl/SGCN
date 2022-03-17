dcc7a8180a135d398e803b72b483e659b2ab62fd
zyq <zhouyq@mail.nankai.edu.cn>
2022-03-17 19:18:07 +0800

    annotation for dataset

--- utils.py
+++ utils.py
@@ -13,33 +13,39 @@ def anorm(p1, p2):
 
 def loc_pos(seq_):
 
-    # seq_ [obs_len N 2]
+    # seq_ [obs_len pedsN 2]
 
     obs_len = seq_.shape[0]
     num_ped = seq_.shape[1]
-
+    # 1D, [1:obs_len]
     pos_seq = np.arange(1, obs_len + 1)
+    # expand to 3D (obs_len, 1, 1)
     pos_seq = pos_seq[:, np.newaxis, np.newaxis]
+    # (obs_len, 2, 1)
     pos_seq = pos_seq.repeat(num_ped, axis=1)
-
+    # (obs_len, num_ped, 3), result[x,:,0] = x+1
     result = np.concatenate((pos_seq, seq_), axis=-1)
-
     return result
 
 def seq_to_graph(seq_, seq_rel, pos_enc=False):
+    # (valid_peds_num_in_this_seq, 2, frame(8 or 12))
+    # squeeze: remove dimension if is 1
     seq_ = seq_.squeeze()
     seq_rel = seq_rel.squeeze()
     seq_len = seq_.shape[2]
     max_nodes = seq_.shape[0]
-
+    # (frameNum, pedsNum, 2)
     V = np.zeros((seq_len, max_nodes, 2))
     for s in range(seq_len):
+        # frame S (pedsNum,2)
         step_ = seq_[:, :, s]
         step_rel = seq_rel[:, :, s]
         for h in range(len(step_)):
+            # V(frameID, pedID, 2)
             V[s, h, :] = step_rel[h]
 
     if pos_enc:
+        # obs_len, num_ped, 3), result[x, :, 0] = x + 1
         V = loc_pos(V)
 
     return torch.from_numpy(V).type(torch.float)
@@ -54,7 +60,8 @@ def poly_fit(traj, traj_len, threshold):
     - int: 1 -> Non Linear 0-> Linear
     """
     t = np.linspace(0, traj_len - 1, traj_len)
-    res_x = np.polyfit(t, traj[0, -traj_len:], 2, full=True)[1]
+    # polyfit returns a set of values represented the correlation between two number series if full=True
+    res_x = np.polyfit(t, traj[0, -traj_len:], 2, full=True)[1] #NOTE this syntax
     res_y = np.polyfit(t, traj[1, -traj_len:], 2, full=True)[1]
     if res_x + res_y >= threshold:
         return 1.0
@@ -64,15 +71,18 @@ def poly_fit(traj, traj_len, threshold):
 
 def read_file(_path, delim='\t'):
     data = []
+    # match tab
     if delim == 'tab':
         delim = '\t'
     elif delim == 'space':
         delim = ' '
     with open(_path, 'r') as f:
         for line in f:
+            # strip: remove space or newline characters at the beginning and the end of the string
             line = line.strip().split(delim)
             line = [float(i) for i in line]
             data.append(line)
+    # a little different from np.array
     return np.asarray(data)
 
 class TrajectoryDataset(Dataset):
@@ -113,19 +123,23 @@ class TrajectoryDataset(Dataset):
 
         for path in all_files:
             data = read_file(path, delim)
-
-            frames = np.unique(data[:, 0]).tolist()
+            # unique: remove duplicates in list
+            frames = np.unique(data[:, 0]).tolist() # all frame id
             frame_data = []
             for frame in frames:
-                frame_data.append(data[frame == data[:, 0], :])
+                # (exp) frame_data[0]: all data in frame 0
+                frame_data.append(data[frame == data[:, 0], :]) # NOTE the use of this syntax
             num_sequences = int(
                 math.ceil((len(frames) - self.seq_len + 1) / skip))
 
             for idx in range(0, num_sequences * self.skip + 1, skip):
+                # concatenate all data in one trajectory(20 frames) by row (keep col unchanged)
                 curr_seq_data = np.concatenate(
                     frame_data[idx:idx + self.seq_len], axis=0)
+                # all peds id in current trajectory
                 peds_in_curr_seq = np.unique(curr_seq_data[:, 1])
                 self.max_peds_in_frame = max(self.max_peds_in_frame, len(peds_in_curr_seq))
+                # (peds_num, 2(feature), obs+pred len)
                 curr_seq_rel = np.zeros((len(peds_in_curr_seq), 2,
                                          self.seq_len))
                 curr_seq = np.zeros((len(peds_in_curr_seq), 2, self.seq_len))
@@ -134,16 +148,20 @@ class TrajectoryDataset(Dataset):
                 num_peds_considered = 0
                 _non_linear_ped = []
                 for _, ped_id in enumerate(peds_in_curr_seq):
+                    # all data for each ped in current seq (frame_id, ped_id, pos_X, pos_Y)
                     curr_ped_seq = curr_seq_data[curr_seq_data[:, 1] ==
                                                  ped_id, :]
                     curr_ped_seq = np.around(curr_ped_seq, decimals=4)
+                    # the first frame of this ped's appearance (only considering this seq, maximum 20)
                     pad_front = frames.index(curr_ped_seq[0, 0]) - idx
                     pad_end = frames.index(curr_ped_seq[-1, 0]) - idx + 1
+                    # the ped should appear in all frames of one seq
                     if pad_end - pad_front != self.seq_len:
                         continue
-                    curr_ped_seq = np.transpose(curr_ped_seq[:, 2:])
+                    # transpose this matrix, remain all 20 rows and cal[2:], from(20, 4) to (2,20)
+                    curr_ped_seq = np.transpose(curr_ped_seq[:, 2:]) # NOTE this syntax
                     curr_ped_seq = curr_ped_seq
-                    # Make coordinates relative
+                    # Make coordinates relative, (2,20), the relative position of each frame and this previous frame
                     rel_curr_ped_seq = np.zeros(curr_ped_seq.shape)
                     # ipdb.set_trace()
                     rel_curr_ped_seq[:, 1:] = \
@@ -151,30 +169,40 @@ class TrajectoryDataset(Dataset):
                     # rel_curr_ped_seq[:, 1:] = \
                     #     curr_ped_seq[:, 1:] - np.reshape(curr_ped_seq[:, 0], (2,1))
                     _idx = num_peds_considered
+                    # curr_seq(all_peds_num, 2, 20), data in curr_seq[valid_peds_num:all_peds_num] is zero
                     curr_seq[_idx, :, pad_front:pad_end] = curr_ped_seq
                     curr_seq_rel[_idx, :, pad_front:pad_end] = rel_curr_ped_seq
                     # Linear vs Non-Linear Trajectory
+                    # 1 if the ped's trajectory is non-linear
                     _non_linear_ped.append(
                         poly_fit(curr_ped_seq, pred_len, threshold))
                     curr_loss_mask[_idx, pad_front:pad_end] = 1
                     num_peds_considered += 1
 
+                # have traversed all peds in curr seq
                 if num_peds_considered > min_ped:
+                    # +=: concat to the end
                     non_linear_ped += _non_linear_ped
                     num_peds_in_seq.append(num_peds_considered)
                     loss_mask_list.append(curr_loss_mask[:num_peds_considered])
+                    #seq_list(seq_num, valid_ped_in_this_seq, 2, 20)
                     seq_list.append(curr_seq[:num_peds_considered])
                     seq_list_rel.append(curr_seq_rel[:num_peds_considered])
 
         self.num_seq = len(seq_list)
+        # concatenate to 3 dimension (valid_ped_in_all_seq, 2, 20)
         seq_list = np.concatenate(seq_list, axis=0)
         seq_list_rel = np.concatenate(seq_list_rel, axis=0)
+        # ones(all_peds_num, 20)
         loss_mask_list = np.concatenate(loss_mask_list, axis=0)
+        # 1D, all peds' linear nature (0->linear 1->nonlinear)
         non_linear_ped = np.asarray(non_linear_ped)
 
         # Convert numpy -> Torch Tensor
+        # (valid_ped_in_all_seq, 2, 8)
         self.obs_traj = torch.from_numpy(
             seq_list[:, :, :self.obs_len]).type(torch.float)
+        # (valid_ped_in_all_seq, 2, 12)
         self.pred_traj = torch.from_numpy(
             seq_list[:, :, self.obs_len:]).type(torch.float)
         self.obs_traj_rel = torch.from_numpy(
@@ -183,7 +211,10 @@ class TrajectoryDataset(Dataset):
             seq_list_rel[:, :, self.obs_len:]).type(torch.float)
         self.loss_mask = torch.from_numpy(loss_mask_list).type(torch.float)
         self.non_linear_ped = torch.from_numpy(non_linear_ped).type(torch.float)
+        # cumsum: return the cumulative sum of the elements along the gicen axis;
+        # cum_start idx: the start cum id of ped in each seq (start from 0)
         cum_start_idx = [0] + np.cumsum(num_peds_in_seq).tolist()
+        # start cum id, end cum id in each seq
         self.seq_start_end = [
             (start, end)
             for start, end in zip(cum_start_idx, cum_start_idx[1:])
@@ -197,7 +228,8 @@ class TrajectoryDataset(Dataset):
             pbar.update(1)
 
             start, end = self.seq_start_end[ss]
-
+            # obs_traj: (all_valid_peds_num, 2, frames(8)), pass valid peds of each seq into <seqtograph>
+            # v: (obj_len, peds_num, feature(3))m feature[0]=frame_id+1, feature[1] and future[2] = rel_pos
             v_= seq_to_graph(self.obs_traj[start:end, :], self.obs_traj_rel[start:end, :], True)
             self.v_obs.append(v_.clone())
             v_= seq_to_graph(self.pred_traj[start:end, :], self.pred_traj_rel[start:end, :], False)
